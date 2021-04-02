510088817b62bb01077ef5252e111ea7b220c1a6
Tim Roith <44805883+TimRoith@users.noreply.github.com>
2021-04-02 23:20:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import os
 import torch
-from utils.configuration import Conf
+import utils.configuration as cf
 from utils.datasets import get_data_set
 import models
-from train import train_step, validation_step, test_step
+import train
 import adversarial_attacks as at
 
 from torchvision import datasets, transforms
@@ -11,88 +10,62 @@ from torch.utils.data import DataLoader
 
 import matplotlib.pyplot as plt
 
+# -----------------------------------------------------------------------------------
+# Set up variable and data for an example
+# -----------------------------------------------------------------------------------
+# specify the path of your data
 data_file = "/"
 
-conf_args = {'lamda':0.01,'data_file':data_file, 'use_cuda':True, 'train_split':0.9, 'num_workers':4,
-             'regularization': "global_lipschitz", 'reg_init': "partial_random",'reg_lr':10,
-             'activation_function':"sigmoid"}
-conf = Conf(**conf_args)
+# load up configuration from examples
+conf = cf.pgd_example(data_file, use_cuda=True)
 
-
-#%% get train, validation and test loader
-
+# get train, validation and test loader
 train_loader, valid_loader, test_loader = get_data_set(conf)
 
-# # %% deine the model
-model = models.fully_connected([784, 64, 64, 10], conf.activation_function)
-best_model = models.fully_connected([784, 64, 64, 10], conf.activation_function)
-model.to(conf.device)
-best_model.to(conf.device)
 
-# define the adverserial attack
-#conf.attack = at.fgsm(model, conf.loss)
-conf.attack = at.pgd(model, conf.loss)
+# -----------------------------------------------------------------------------------
+# define the model and an instance of the best model class
+# -----------------------------------------------------------------------------------
+model = models.fully_connected([784, 64, 64, 10], conf.activation_function).to(conf.device)
+best_model = train.best_model(models.fully_connected([784, 64, 64, 10], conf.activation_function).to(conf.device))
 
+# -----------------------------------------------------------------------------------
+# Initialize optimizer and lamda scheduler
+# -----------------------------------------------------------------------------------
+opt = torch.optim.SGD(model.parameters(), lr = 0.1, momentum =0.9)
+lamda_scheduler = train.lamda_scheduler(conf, warmup = 5, warmup_lamda = 0.0, cooldown = 1)
+# -----------------------------------------------------------------------------------
 
+# -----------------------------------------------------------------------------------
+# initalize history
+# -----------------------------------------------------------------------------------
+tracked = ['train_loss', 'train_acc', 'train_lip_loss', 'val_loss', 'val_acc']
+history = {key: [] for key in tracked}
+# -----------------------------------------------------------------------------------
+# cache for the lipschitz update
+cache = {'counter':0}
 
 print("Train model: {}".format(conf.model))
-opt = torch.optim.SGD(model.parameters(), lr = 0.1, momentum =0.9)
-
-# initalize history
-history = {}
-history['train_loss'] = []
-history['train_acc'] = []
-history['train_lip_loss'] = []
-history['val_loss'] = []
-history['val_acc'] = []
-history['val_lip_loss'] = []
-best_acc = 0.0
-
-cache = {'idx':0}
-train_data = {}
-
-train_data['u'] = torch.tensor((1, *conf.im_shape)).to(conf.device)
-train_data['v'] = torch.tensor((1, *conf.im_shape)).to(conf.device)
-
-
-
-
 for i in range(conf.epochs):
     print(25*"<>")
     print(50*"|")
     print(25*"<>")
     print('Epoch', i)
     
-    if i % 5 == 0:
-        train_data['u'] = None
     # train_step
-    train_data = train_step(conf, model, opt, train_loader, valid_loader, u = train_data['u'], v = train_data['v'], cache = cache)
+    train_data = train.train_step(conf, model, opt, train_loader, valid_loader, cache)
     
-    cache = train_data['cache']
-    # get adverserial pair from train_step
-    idx = train_data['highest_loss_idx']
-    u_reg = train_data['u'][idx:idx+1]
-    v_reg = train_data['v'][idx:idx+1]
-    
-    plt.matshow(u_reg[0,0,::].cpu().detach())
-    plt.matshow(v_reg[0,0,::].cpu().detach())
-    plt.show()
-    
+    # ------------------------------------------------------------------------
     # validation step
-    val_data = validation_step(conf, model, valid_loader, u_reg, v_reg)
-    
+    val_data = train.validation_step(conf, model, valid_loader)
     
     # ------------------------------------------------------------------------
     # update history
-    history['train_loss'].append(train_data['train_loss'])
-    history['train_acc'].append(train_data['train_acc'])
-    history['train_lip_loss'].append(train_data['train_lip_loss'])
-    #
-    history['val_loss'].append(val_data['val_loss'])
-    history['val_acc'].append(val_data['val_acc'])
-    history['val_lip_loss'].append(val_data['val_lip_loss'])
+    for key in tracked:
+        if key in val_data:
+            history[key].append(val_data[key])
+        if key in train_data:
+            history[key].append(train_data[key])
     
-    if val_data['val_acc']> best_acc:
-        print("Found better model.")
-        best_acc = val_data['val_acc']
-        best_model.load_state_dict(model.state_dict())
+    # ------------------------------------------------------------------------
+    lamda_scheduler(conf, train_data['train_acc'])
