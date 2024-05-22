f05febcc67eca84a294006017ea3580f3a9b0826
RaphaelB-Ensta <bernasraphael@gmail.com>
2024-05-22 11:55:41 +0200

    update train_regularizer

--- train_regularizer.py
+++ train_regularizer.py
@@ -20,16 +20,21 @@ class Polynom:
 
     def createdata(self, x):
         y = self(x) + torch.randn_like(x) * 0.1
-        return torch.stack((x, y), dim=1)
+        xy = torch.stack((x, y), dim=1)
+        dataset = torch.utils.data.TensorDataset(xy[:, 0], xy[:, 1])
+        loader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
+        return loader, xy
 
-    def plot(self):
+    def plot(self, ax=None):
         x = torch.linspace(-1, 1, 100)
-        y = self.createdata(x).cpu().detach()
-        plt.plot(y)
-        plt.show()
+        y = self.createdata(x)[1].cpu().detach()[:, 1]
+        if ax is None:
+            plt.plot(y)
+        else:
+            ax.plot(y)
 
 class Trainer:
-    def __init__(self, model, train_loader, lip_reg_max, lamda=0.1, lr=0.1, adversarial_name="gradient_ascent", num_iters=1000):
+    def __init__(self, model, train_loader, lip_reg_max, lamda=0.1, lr=0.1, adversarial_name="gradient_ascent", num_iters=7, epsilon=1e-1):
         self.device = device
         self.model = model.to(self.device)
         self.train_loader = train_loader
@@ -39,6 +44,11 @@ class Trainer:
         self.adversarial = lambda u: adversarial_update(self.model, u, u+torch.rand_like(u)*0.1, opt_kwargs={'name':adversarial_name, 'lr':self.lr})
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.lr)
         self.lamda = lamda
+        self.epsilon = epsilon
+        self.train_acc = 0.0
+        self.train_loss = 0.0
+        self.tot_steps = 0
+        self.train_lip_loss = 0.0
 
     def train_step(self):
         # train phase
@@ -46,10 +56,10 @@ class Trainer:
         opt = self.optimizer
 
         # initialize values for train accuracy and train loss
-        train_acc = 0.0
-        train_loss = 0.0
-        tot_steps = 0
-        train_lip_loss = 0.0
+        self.train_acc = 0.0
+        self.train_loss = 0.0
+        self.tot_steps = 0
+        self.train_lip_loss = 0.0
 
         #
         u = None
@@ -65,19 +75,20 @@ class Trainer:
             # ---------------------------------------------------------------------
 
             # adversarial update on the Lipschitz set
-            adv = self.adversarial(torch.tensor([x.item()]).to(self.device))
+            adv = self.adversarial(x)
             for _ in range(self.num_iters):
                 adv.step()
             u, v = adv.u, adv.v
             # ---------------------------------------------------------------------
 
             # Compute the Lipschitz constant
-            c_reg_loss = lip_constant_estimate(self.model)(u, v)
+            c_reg_loss = lip_constant_estimate(self.model, mean = True)(u, v)
             # reset gradients
             opt.zero_grad()
 
             # evaluate model on batch
-            logits = self.model(x.unsqueeze(0))
+            logits = self.model(x)
+            logits = logits.reshape(logits.shape[0])
 
             # Get classification loss
             c_loss = F.mse_loss(logits, y)
@@ -96,33 +107,49 @@ class Trainer:
             opt.step()
 
             # update accuracy and loss
-            train_acc += (logits.item() == y).sum().item()
-            train_loss += c_loss.item()
-            train_lip_loss = max(c_reg_loss.item(), train_lip_loss)
-            tot_steps += 1
-
-        return {'train_loss': train_loss, 'train_acc': train_acc / tot_steps,
-                'train_lip_loss': train_lip_loss}
-
-    def plot(self):
+            self.train_loss += c_loss.item()
+            self.train_lip_loss = max(c_reg_loss.item(), self.train_lip_loss)
+            for i in range(y.shape[0]):
+                self.tot_steps += 1
+                equal = torch.isclose(logits[i], y[i], atol=self.epsilon)
+                self.train_acc += equal.item()*1.0
+        self.train_acc /= self.tot_steps
+    def plot(self, ax=None):
         x = torch.linspace(-1, 1, 100).to(device)
         y = self.model(x.t()).cpu().detach()
-        plt.plot(y)
-        plt.show()
+        if ax is None:
+            plt.plot(y)
+        else:
+            ax.plot(y)
 
 if __name__ == "__main__":
+    plt.close('all')
+    fig, ax = plt.subplots(1,)
     coeffs = [1., 2., 3.]
     polynom = Polynom(coeffs)
     polynom.plot()
     x = torch.linspace(-1, 1, 100).to(device)
-    xy = polynom.createdata(x).to(device)
+    xy_loader = polynom.createdata(x)[0]
     model = fully_connected([1, 10, 10, 1], "ReLu")
     model = model.to(device)
-    trainer = Trainer(model, xy, 10, lamda=0.1, lr=0.1, adversarial_name="SGD", num_iters=1000)
+    ax.plot(x.cpu(),model(x).cpu().detach())
+    trainer = Trainer(model, xy_loader, 10, lamda=0.1, lr=0.1, adversarial_name="SGD", num_iters=1000)
     trainer.plot()
-    for i in range(100):
+    plt.show()
+    num_total_iters = 5
+    for i in range(num_total_iters):
         trainer.train_step()
-        if i % 10 == 0:
+        if i % 1 == 0:
             print(i)
-    trainer.plot()
-    print(coeffs)
+            ax.set_title('Iteration: ' + str(i))
+            print("train accuracy : ", trainer.train_acc)
+            print("train loss : ", trainer.train_loss)
+            print("train lip loss : ", trainer.train_lip_loss)
+            polynom.plot(ax=ax)
+            trainer.plot(ax=ax)
+            plt.pause(0.1)
+    ax.set_title('Iteration: ' + str(num_total_iters))
+    polynom.plot(ax=ax)
+    trainer.plot(ax=ax)
+    ax.legend(["True", "Model"])
+    plt.show()
